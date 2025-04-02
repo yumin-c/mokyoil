a4825e37ef490ac51f4f22e12fcf1982ba610459
Yumin Cheong <yumin.c@yonsei.ac.kr>
2025-04-03 08:15:02 +0900

    Initial Commit

---
+++ mokyoilTests/mokyoilTests.swift
@@ -0,0 +1,17 @@
+//
+//  mokyoilTests.swift
+//  mokyoilTests
+//
+//  Created by Yumin on 4/3/25.
+//
+
+import Testing
+@testable import mokyoil
+
+struct mokyoilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
